e5a3b2bb8f7908c22465d498f2f31ac1d02fa24a
Yasir Gaji <gajiyasir@gmail.com>
2023-06-14 16:12:19 +0100

    used the context to pull in state data functions

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,31 @@ import React, { useContext } from "react";
 
 import { Context } from "../context";
 import { useRouter } from "next/dist/client/router";
+import axios from "axios";
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const {
+    username,
+    setUsername,
+    secret,
+    setSecret,
+  } = useContext(Context)
+
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form className="auth-form" onSubmit={e => e.preventDefault()}>
+          <div className="auth-title">Chat App</div>
+
+          <div className="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={e => setUsername(e.target.value)}
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
